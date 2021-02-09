952fd7101e851dfe27ef1c90c0215472a80bec47
PravallikaVissamsetti <68841211+PravallikaVissamsetti@users.noreply.github.com>
2021-02-09 15:02:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import { Text, View, Button } from 'react-native'; 
+import 'react-native-gesture-handler'; 
+import NavScreens from './Petfit/NavScreens'; 
+const App = () => { 
+  return ( 
+    <> 
+      <NavScreens /> 
+    </> 
+  ); 
+} 
+export default App; 
